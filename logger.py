ede602112ba77a37a668709f9b088727d19e5bd2
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-17 11:39:44 +0300

    init time branch

--- logger.py
+++ logger.py
@@ -4,12 +4,12 @@ import os
 LOG_FILE = "/data/log.txt"
 os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
 
-with open(LOG_FILE, "a") as f:
-    while True:
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-        f.write(f"[{timestamp}]\n")
-        print(f"New log in {LOG_FILE}")
-        try:
-            time.sleep(3)
-        except KeyboardInterrupt:
-            break
+while True:
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"New log in {LOG_FILE}")
+    try:
+        with open(LOG_FILE, "a") as f:
+            f.write(f"[{timestamp}]\n")
+        time.sleep(3)
+    except KeyboardInterrupt:
+        break
