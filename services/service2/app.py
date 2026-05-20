fc6afdffc00534f37408b251922ed14b23160b8d
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-20 15:35:50 +0300

    feat: add request from service 2 to service 3

--- services/service2/app.py
+++ services/service2/app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    response = requests.get('http://service1:5001/info')
-    return f"Service 2 says: {response.json()}"
+    response_service1 = requests.get('http://service1:5001/info')
+    response_service3 = requests.get('http://service3:5003/rand')
+    
+    answ_text = f"Service 1 says : {response_service1}\nService 3 says : {response_service3}"
+
+    return answ_text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
