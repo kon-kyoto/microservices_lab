e01852ab638c375d69eecdb26e9954401a21d96a
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-20 17:09:11 +0300

    reboot: proj wuth simple services moved to dev_simle_services branch

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:latest
-WORKDIR /app
-RUN python -m venv /opt/venv
-ENV PATH="/opt/venv/bin:$PATH"
-RUN pip install flask
-COPY app.py .
-CMD ["python", "app.py"]
--- services/service1/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/info')
-def info():
-    return jsonify({'message': 'It is first service', 'port': 5001})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
