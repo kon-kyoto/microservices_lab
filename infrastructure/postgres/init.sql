1c47679f47aa1ac82af38d660243cc169fa395d6
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-20 17:35:26 +0300

    feat: init database anatomy

---
+++ infrastructure/postgres/init.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE auth_db;
+CREATE DATABASE users_db;
+CREATE DATABASE orders_db;
+
+\c users_db;
+CREATE TABLE users {
+}
+
+\c auth_db;
+CREATE TABLE users {
+}
+
+\c orders_db;
+CREATE TABLE users {
+}
