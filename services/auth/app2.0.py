d3ed4eb1711d433b294174f515b45ed8bd8f81c4
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-25 18:30:49 +0300

    feat: change some fail

--- services/auth/app2.0.py
+++ services/auth/app2.0.py
@@ -1,16 +1,18 @@
 import os
-from dotenv import load_dotenv()
+from dotenv import load_dotenv
 
+from flask import Flask, request, jsonify
 import bcrypt
 
-import psycopq2
-from psycopq2.extras import RealDictCursor
+import psycopg2
+from psycopg2.extras import RealDictCursor
 from contextlib import contextmanager
 
 load_dotenv()
+app = Flask(__name__)
 
 def get_db_connection():
-    return psycopq2.connect(
+    return psycopg2.connect(
             host=os.getenv('DB_HOST'),
             database=os.getenv('AUTH_DB_NAME'),
             user=os.getenv('DB_USER'),
@@ -35,19 +37,22 @@ def register():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    email = daya.get('email')
+    email = data.get('email')
 
-    password_hash = bcrypt.hashow(password.encode('utf-8'), bcrypt.gensalt())r
+    password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     try:
         with get_db_cursor() as cur:
             cur.execute(
-                    "INSERT INTO users (username, email, password_hash) VALUES (%s %s %s) RETURING id",
+                    "INSERT INTO users (username, email, password_hash) VALUES (%s %s %s) RETURNING id",
                     (username, email, password_hash.decode('utf-8'))
                 )
 
             user_id = cur.fetchone()['id']
 
             return jsonify({'message': 'user created', 'user_id': user_id}), 201
-    except psycopq2.InegrityError:
+    except psycopg2.IntegrityError:
         return jsonify({"error": "username or email is already exist"}), 400
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0",  port = 5000)
