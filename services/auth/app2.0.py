6288b8cf41cf57a0715b0f4952f90cd009e03fbf
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-25 17:47:45 +0300

    feat: add func for registr

--- services/auth/app2.0.py
+++ services/auth/app2.0.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv()
 
+import bcrypt
+
 import psycopq2
 from psycopq2.extras import RealDictCursor
 from contextlib import contextmanager
@@ -27,3 +29,25 @@ def get_db_cursor():
         raise
     finally:
         conn.close()
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    email = daya.get('email')
+
+    password_hash = bcrypt.hashow(password.encode('utf-8'), bcrypt.gensalt())r
+
+    try:
+        with get_db_cursor() as cur:
+            cur.execute(
+                    "INSERT INTO users (username, email, password_hash) VALUES (%s %s %s) RETURING id",
+                    (username, email, password_hash.decode('utf-8'))
+                )
+
+            user_id = cur.fetchone()['id']
+
+            return jsonify({'message': 'user created', 'user_id': user_id}), 201
+    except psycopq2.InegrityError:
+        return jsonify({"error": "username or email is already exist"}), 400
