e35f00cee70e04972eae6d3bfb76839098e3bf9c
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-26 14:07:59 +0300

    feat: add permission checker and server error

--- services/users/app.py
+++ services/users/app.py
@@ -36,8 +36,7 @@ def get_db_cursor():
 
 @app.route('/user/<find_id>', methods=['GET'])
 def get_user(find_id):
-    data = request.get_json()
-    token = data.get('token')
+    token = request.header.get("Authorization").replace("Bearer ", "")
 
     try:
         jwt_data = jwt.decode(
@@ -45,6 +44,10 @@ def get_user(find_id):
                 os.getenv('SECRET_KEY'), 
                 os.getenv('JWT_ALGORITHM')
             )
+
+        if jwt_data['user_id'] != find_id:
+            return jsonify({'message':'permission denied'})
+
         
         with get_db_cursor() as cur:
             cur.execute(
@@ -53,14 +56,17 @@ def get_user(find_id):
                 )
             data_row = cur.fetchone()
             if not data_row:
-                return jsonify({'message': 'user not found'}), 401
+                return jsonify({'message': 'user not found'}), 404
 
         return jsonify({'username': data_row['username'], 'email': data_row['email']}), 200
 
     except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'token is dead'}), 400
+        return jsonify({'message': 'token is dead'}), 401
     except jwt.InvalidTokenError:
-        return jsonify({'message': 'token is invalid'}), 400
+        return jsonify({'message': 'token is invalid'}), 401
+    except Exception as e:
+        app.logger.error(f"[ERROR] {str(e)}")
+        return jsonify({'message':'server error'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001')
