901e6e786fe20f2bf4a8465c5bac813317f54360
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-27 22:42:17 +0300

    feat: rate limit fix

--- services/auth/app.py
+++ services/auth/app.py
@@ -114,7 +114,7 @@ def login():
     if att > 10:
         return jsonify({'message': 'too much try'}), 401
 
-    redis_client.expire(f"login_rate:{request.remote_addr}", os.getenv('STOP_LOGIN'))
+    redis_client.expire(f"login_rate:{request.remote_addr}", int(os.getenv('STOP_LOGIN')))
 
     try:
         with get_db_cursor() as cur:
