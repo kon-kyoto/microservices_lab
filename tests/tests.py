e44ca6436ecb0162679099d346a6b71f0b141440
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-30 04:11:10 +0300

    feat: adapt for users tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from auth_tests import gen_users, register_user, login_user, verify_user
+from users_tests import user_info, user_change_username, user_change_email, users_list, user_delete
 
 users = gen_users()
 
@@ -8,6 +9,11 @@ def tests(user):
     assert register_user(user)
     assert login_user(user)
     assert verify_user(user)
+    assert users_list(user)
+    assert user_info(user)
+    assert user_change_username(user)
+    assert user_change_email(user)
+    assert user_delete(user)
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
