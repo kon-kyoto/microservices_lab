319bcd8b1772e337d7b5e5e9847c79449711a50c
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-31 00:56:33 +0300

    feat: fix redis password error

--- services/auth/app.py
+++ services/auth/app.py
@@ -33,8 +33,8 @@ else:
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT")),
-    password=os.getenv("REDIS_PASSWORD")
-    decode_responses=True,
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True
 )
 
 JWT_CONFIG = {
