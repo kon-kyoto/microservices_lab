23a0badf549ffe77c2f8c5fc5e8b6ec76a44e7fc
kon-kyoto <kyotodanmaster@gmail.com>
2026-05-30 16:59:50 +0300

    feat: password for redis

--- services/auth/app.py
+++ services/auth/app.py
@@ -33,6 +33,7 @@ else:
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT")),
+    password=os.getenv("REDIS_PASSWORD")
     decode_responses=True,
 )
 
